29acff9861b51e8a69e0dcf8498ccbbf427c96d9
DHZK2 <k2guffy@gmail.com>
2023-05-27 14:44:10 -0500

    Diseño de los cards para los productos, y  archivo para el css

--- EjercicioCarrito/src/componentes/Producto.jsx
+++ EjercicioCarrito/src/componentes/Producto.jsx
@@ -6,19 +6,22 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
+import "./Styles.css";
+
 const Producto = ({infoProducto}) =>{
+
 
     console.log(infoProducto);
     return (
             <>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 345 }} className="item">
                 <CardMedia sx={{ height: 140 }}>
                     <img src={infoProducto.image} alt={infoProducto.nombre} sx={{maxWith:100,  height: 140}}  />
                 </CardMedia >
                 <CardContent>
-                    <h3>{infoProducto.nombre}</h3>
-                    <p>{infoProducto.precio}</p>
-                    <p>{infoProducto.descripcion}</p>
+                    <h3 className="name">{infoProducto.nombre}</h3>
+                    <p className="price">{infoProducto.precio}</p>
+                    <p className="description">{infoProducto.descripcion}</p>
                 </CardContent>
                 <CardActions>
                     <Button variant="contained" color="secondary" startIcon={<AddShoppingCartIcon />}>Agregar al carrito</Button>
