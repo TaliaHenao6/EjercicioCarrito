1f6d2827d0251feb86a84c9bd6ed03bdf45dcdf6
DHZK2 <k2guffy@gmail.com>
2023-05-27 14:27:31 -0500

    Diseño del producto, importacion de las imagenes en json

--- EjercicioCarrito/src/componentes/Producto.jsx
+++ EjercicioCarrito/src/componentes/Producto.jsx
@@ -7,13 +7,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
 const Producto = ({infoProducto}) =>{
-    
+
+    console.log(infoProducto);
     return (
             <>
-            <Card >
-                <CardMedia>
-                    <img src="" alt={infoProducto.nombre} />
-                </CardMedia>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia sx={{ height: 140 }}>
+                    <img src={infoProducto.image} alt={infoProducto.nombre} sx={{maxWith:100,  height: 140}}  />
+                </CardMedia >
                 <CardContent>
                     <h3>{infoProducto.nombre}</h3>
                     <p>{infoProducto.precio}</p>
