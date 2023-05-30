ddcddbca24777919b6d7e0fed5729e444ae47356
Nicolassar15 <nicolassar15@gmail.com>
2023-05-29 19:26:58 -0500

    agregando favorito

--- EjercicioCarrito/src/componentes/Heart.jsx
+++ EjercicioCarrito/src/componentes/Heart.jsx
@@ -1,33 +1,34 @@
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
-import  {useState}  from "react";
+import { useState } from "react";
 
 import "./Styles.css";
 
-const Heart =()=>{
+const Heart = () => {
+  const [like, setLike] = useState(false);
 
-    const [like, setLike] = useState(false);
+  const handleClick = () => {
+    if (like == true) {
+      setLike(false);
+      // console.log(`se quito de favoritos! ${infoProducto.nombre}`);
+    } else {
+      setLike(true);
 
-    const handleClick = () => {
-      
-        if(like == true){
-            setLike(false);
-            // console.log(`se quito de favoritos! ${infoProducto.nombre}`);
-        }else{
-            setLike(true);
-            
-            // console.log(`Se agrego a favoritos ${infoProducto.nombre}`);
-        }
-        // Operador ternarios
-        // like == true ? setLike(false) : setLike(true)
+      // console.log(`Se agrego a favoritos ${infoProducto.nombre}`);
     }
-    return(
-        <>
-            <div className="liked" color="primary" onClick={handleClick}>
-                {like == true ? <Favorite color="error"/> :<FavoriteBorder color="error" /> }
-            </div>
-        </>
-    )
-
-}
+    // wishlist
+    // like == true ? setLike(false) : setLike(true)
+  };
+  return (
+    <>
+      <div className="liked" color="primary" onClick={handleClick}>
+        {like == true ? (
+          <Favorite color="error" />
+        ) : (
+          <FavoriteBorder color="error" />
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Heart;
