e28ad148d63459a91ed2b2e11a6ebb346489fa12
DHZK2 <k2guffy@gmail.com>
2023-05-31 15:37:10 -0500

    Diseño del login

--- EjercicioCarrito/src/componentes/Login.jsx
+++ EjercicioCarrito/src/componentes/Login.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import md5 from "md5";
 import Cookies from "universal-cookie";
 
-import './styles.css';
+import {Button} from "@mui/material"
+
+import "./styles.css";
 
 const baseUrl = "http://localhost:3001/usuarios";
-const cookies = new Cookies;
+const cookies = new Cookies();
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
   const [errors, setErrors] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
-  const [successMessage, setSuccessMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -27,13 +29,13 @@ const Login = () => {
     e.preventDefault();
     const newErrors = validateForm(formData);
     if (Object.keys(newErrors).length === 0) {
-      setSuccessMessage('Sesión iniciada con éxito');
+      setSuccessMessage("Sesión iniciada con éxito");
       setFormData({
-        email: '',
-        password: ''
+        email: "",
+        password: "",
       });
       setTimeout(() => {
-        setSuccessMessage('');
+        setSuccessMessage("");
       }, 3000);
     } else {
       setErrors(newErrors);
@@ -44,17 +46,16 @@ const Login = () => {
     const errors = {};
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
     if (!data.email) {
-      errors.email = 'Por favor, ingresa tu correo electrónico';
+      errors.email = "Por favor, ingresa tu correo electrónico";
     } else if (!emailRegex.test(data.email)) {
-      errors.email = 'Por favor, ingresa un correo electrónico válido';
+      errors.email = "Por favor, ingresa un correo electrónico válido";
     }
 
     if (!data.password) {
-      errors.password = 'Por favor, ingresa tu contraseña';
+      errors.password = "Por favor, ingresa tu contraseña";
     } else if (data.password.length < 6) {
-      errors.password = 'La contraseña debe tener al menos 6 caracteres';
+      errors.password = "La contraseña debe tener al menos 6 caracteres";
     }
 
     return errors;
@@ -62,77 +63,84 @@ const Login = () => {
 
   // Iniciar sesion
   const login = async () => {
-      await axios.get( baseUrl, {params: {email: formData.email, password: formData.password}})
-      .then(response => {
+    await axios
+      .get(baseUrl, {
+        params: { email: formData.email, password: formData.password },
+      })
+      .then((response) => {
         console.log(response.data);
-        return response.data
-    })
-    .then( response =>{
-      if(response.length > 0){
-        const  respuesta = response[0];
-        localStorage.setItem("login", true);
-        localStorage.setItem("userId", respuesta.id);
-        alert(`Bievenido: ${respuesta.name}`);
-        window.location.href ="/";
-      }else{
-        alert("usuario  Y/O contraseña son incorrectos");
-      }
+        return response.data;
+      })
+      .then((response) => {
+        if (response.length > 0) {
+          const respuesta = response[0];
+          localStorage.setItem("login", true);
+          localStorage.setItem("userId", respuesta.id);
+          alert(`Bievenido: ${respuesta.name}`);
+          window.location.href = "/";
+        } else {
+          alert("usuario  Y/O contraseña son incorrectos");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    })
-    .catch(error => {
-      console.log(error);
-    })
-
-      // // axios.post('https://localhost:5001/api/login', body)
-      // //   .then((response) => {
-    }
-  
+    // // axios.post('https://localhost:5001/api/login', body)
+    // //   .then((response) => {
+  };
 
   return (
     <section className="main-container">
-
-    <div className="container">
-    <h1>Iniciar Sesión</h1>
-      <form onSubmit={handleSubmit}>
-       
-        <div className="form-group">
-          <label htmlFor="email">Correo Electrónico:</label><br />
-          <input className='input'
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder='Escribe tu correo electrónico'
+      <div className="container">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form-group">
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input
+              className="input"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Escribe tu correo electrónico"
             />
-          {errors.email && <p className="error-message">{errors.email}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña:</label><br />
-          <input className='input'
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            placeholder='Escribe tu contraseña'
+            {errors.email && <p className="error-message">{errors.email}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              className="input"
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Escribe tu contraseña"
             />
-          {errors.password && <p className="error-message">{errors.password}</p>}
+            {errors.password && (
+              <p className="error-message">{errors.password}</p>
+            )}
+          </div>
+          <div className="form-group">
+            <Button variant="contained" color="secondary" size="small" type="submit" onClick={() => login()}>
+              Iniciar Sesión
+            </Button>
+          </div>
+        </form>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+
+        <div className="info-login">
+        <p className="nous">¿No tienes usuario para iniciar sesión?</p>
+        <a className="btn" href="/Register">
+          Registrarme ahora
+        </a>
+
         </div>
-        <button className='button' type="submit" onClick={()=> login()}>Iniciar Sesión</button>
-        
-      </form>
-      
-      {successMessage && <p className="success-message">{successMessage}</p>}
-
-      <p className="nous">¿No tienes usuario para iniciar sesión?</p>
-      <a className='' href='/Register'>Registrarme ahora</a>
-    </div>
-
+      </div>
     </section>
-    
   );
 };
-
 
 export default Login;
