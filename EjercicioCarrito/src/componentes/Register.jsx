40ac47152d71a3fb0a8c42815736242cfeae538e
DHZK2 <k2guffy@gmail.com>
2023-05-31 19:57:55 -0500

    Registrar

--- EjercicioCarrito/src/componentes/Register.jsx
+++ EjercicioCarrito/src/componentes/Register.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
-import {Button} from "@mui/material"
+import {Button} from "@mui/material";
+
+import axios from "axios";
 import "./styles.css";
+
+const baseUrl = "http://localhost:3001/usuarios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
     phone: "",
-    direccion: "",
+    address: "",
     email: "",
     password: "",
   });
   const [errors, setErrors] = useState({
     name: "",
     phone: "",
-    direccion: "",
+    address: "",
     email: "",
     password: "",
   });
@@ -31,7 +35,7 @@ const Register = () => {
       setFormData({
         name: "",
         phone: "",
-        direccion: "",
+        address: "",
         email: "",
         password: "",
       });
@@ -54,8 +58,8 @@ const Register = () => {
       errors.phone = "Por favor, ingresa tu teléfono";
     }
 
-    if (!data.direccion) {
-      errors.direccion = "Por favor, ingresa tu dirección";
+    if (!data.address) {
+      errors.address = "Por favor, ingresa tu dirección";
     }
 
     if (!data.email) {
@@ -73,91 +77,136 @@ const Register = () => {
     return errors;
   };
 
+
+  const signIn = async () =>{
+    const body = formData;
+    await axios.post(baseUrl, body
+    )
+    .then((response)=>{
+      console.log(response.data);
+      return response.data
+    })
+    .then((response)=>{
+      if (response.length > 0) {
+        const resultado = response[0];
+        localStorage.setItem("login", true);
+        localStorage.setItem("userId", resultado.id);
+        alert(`Bievenido: ${respuesta.name}`);
+        window.location.href = "/";
+        console.log(response);
+      } else {
+        alert("No se puedo hacer el registro");
+      }
+
+    })
+    .catch((error) =>{
+
+    })
+  } 
+
   return (
     <section className="main-container">
-    <div className="container">
-      <h1>Crea una cuenta</h1>
-      <form onSubmit={handleSubmit} className="form">
-        <div className="form-group">
-          <label htmlFor="name">Nombre:</label>
-          <input
-            className="input"
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            placeholder="Escribe tu nombre"
+      <div className="container">
+        <h1>Crea una cuenta</h1>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form-group">
+            <label htmlFor="name">Nombre:</label>
+            <input
+              className="input"
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              placeholder="Escribe tu nombre"
             />
-          {errors.name && <p className="error-message">{errors.name}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Correo Electrónico:</label>
-          <input
-            className="input"
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Escribe tu correo electrónico"
+            {errors.name && <p className="error-message">{errors.name}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input
+              className="input"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Escribe tu correo electrónico"
             />
-          {errors.email && <p className="error-message">{errors.email}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="phone">Telefono:</label>
-          <input
-            className="input"
-            type="number"
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleInputChange}
-            placeholder="Escribe tu teléfono"
+            {errors.email && <p className="error-message">{errors.email}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Telefono:</label>
+            <input
+              className="input"
+              type="number"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+              placeholder="Escribe tu teléfono"
             />
-          {errors.phone && <p className="error-message">{errors.phone}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Dirección:</label>
-          <input
-            className="input"
-            type="text"
-            id="direccion"
-            name="direccion"
-            value={formData.direccion}
-            onChange={handleInputChange}
-            placeholder="Escribe tu dirección"
+            {errors.phone && <p className="error-message">{errors.phone}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Dirección:</label>
+            <input
+              className="input"
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+              placeholder="Escribe tu dirección"
             />
-          {errors.name && <p className="error-message">{errors.direccion}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            className="input"
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            placeholder="Escribe tu contraseña"
+            {errors.name && <p className="error-message">{errors.address}</p>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              className="input"
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Escribe tu contraseña"
             />
-          {errors.password && (
-            <p className="error-message">{errors.password}</p>
+            {errors.password && (
+              <p className="error-message">{errors.password}</p>
             )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="">Rol:</label>
+            <input
+              type="text"
+              className="input"
+              id="rol"
+              name="rol"
+              value="user"
+              disabled={true}
+            />
+          </div>
+          <div className="form-group">
+            <Button
+              variant="contained"
+              size="small"
+              color="secondary"
+              type="submit"
+              onClick={() => signIn()}
+            >
+              Registrarme
+            </Button>
+          </div>
+        </form>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        <div className="form-info">
+          <p className="nous">¿Ya tienes un usuario, quieres iniciar sesión?</p>
+          <a className="btn" type="submit" href="/Login">
+            Iniciar Sesión
+          </a>
         </div>
-        <div className="form-group">
-          <Button variant="contained" size="small" color="secondary" type="submit">
-            Registrarme
-          </Button>
-        </div>
-      </form>
-      {successMessage && <p className="success-message">{successMessage}</p>}
-      <div className="form-info">
-        <p className="nous">¿Ya tienes un usuario, quieres iniciar sesión?</p>
-        <a className='btn' type="submit" href='/Login'>Iniciar Sesión</a>
-
       </div>
-    </div>
     </section>
   );
 };
