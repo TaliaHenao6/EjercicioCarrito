09d94ee0a926c09814a6c9fcd1c660e3b9f0f509
DHZK2 <k2guffy@gmail.com>
2023-06-01 21:36:51 -0500

    Cargando la informacion al perfil

--- EjercicioCarrito/src/componentes/Perfil.jsx
+++ EjercicioCarrito/src/componentes/Perfil.jsx
@@ -1,32 +1,55 @@
-const Perfil = ()=>{
-    return (  
-        <section className="main-container">
-        <div className="container">
-        <h2>My Profile</h2>
-        <div className="">
-          <img src="./src/images/user.png" alt="User" />
-        </div>
-        <div className="">
-          <strong>Nombre:</strong>Talia
-        </div>
-        <div className="">
-          <strong>Teléfono:</strong> 3293894
-        </div>
-        <div className="">
-          <strong>Dirección:</strong> Cartagena
-        </div>
-        <div className="">
-          <strong>Email:</strong> talia.henao@gmail.com
-        </div>
-        <div className="">
-          <strong>Rol:</strong> User
-        </div>
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/usuarios";
+
+const Perfil = () => {
+  const [data, setData] = useState({});
+  const getUser = localStorage.getItem("userId");
+  const idUser = parseInt(getUser);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl, {
+        params: { id: idUser },
+      })
+      .then((response) => {
+        if (response.data.length > 0) {
+          const respuesta = response.data[0];
+          setData(respuesta);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [idUser]);
+
+return (
+  <section className="main-container">
+    <div className="container">
+      <h2>My Profile</h2>
+      <div className="">
+        <img src="./src/images/user.png" alt="User" />
       </div>
-        </section>
-      )
-
-  
-
-}
+      <div className="">
+        <strong>Nombre:</strong>
+        {data.name}
+      </div>
+      <div className="">
+        <strong>Teléfono:</strong> {data.phone}
+      </div>
+      <div className="">
+        <strong>Dirección:</strong> {data.address}
+      </div>
+      <div className="">
+        <strong>Email:</strong> {data.email}
+      </div>
+      <div className="">
+        <strong>Rol:</strong> {data.role}
+      </div>
+    </div>
+  </section>
+);
+};
 
 export default Perfil;
