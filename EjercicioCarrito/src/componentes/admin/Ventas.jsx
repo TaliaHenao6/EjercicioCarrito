ddf7f0404703cfecae995e212c4950704fde5086
Nicolassar15 <nicolassar15@gmail.com>
2023-06-04 21:31:21 -0500

    cambios varios

---
+++ EjercicioCarrito/src/componentes/admin/Ventas.jsx
@@ -0,0 +1,19 @@
+const Ventas = () => {
+  return (
+    <table>
+      <tr>
+        <th>NOMBRE</th>
+        <th>PRECIO</th>
+        <th>CANTIDAD</th>
+      </tr>
+
+      <tr>
+        <td>ESTUFA </td>
+        <td>$12.841.418</td>
+        <td>5</td>
+      </tr>
+      <tr></tr>
+    </table>
+  );
+};
+export default Ventas;
